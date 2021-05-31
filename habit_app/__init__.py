fb4a6c1a4af3740428c539eed17abf3149a604a4
Erin Nordquist <nordquist.erin.ke@gmail.com>
2021-05-31 11:46:21 -0400

    Added CORS

--- habit_app/__init__.py
+++ habit_app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, g
 from config import Config
 from flask_login import LoginManager
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    CORS(app, resources={r'/.*': {'origins': '*'}})
     #if test_config is not None:
 
 
@@ -16,7 +17,7 @@ def create_app(test_config=None):
         #db = init_db()
         init_app(app)
 
-    from . import auth, routes, habit
+    from habit_app import auth, routes, habit
     app.register_blueprint(routes.bp)
     app.register_blueprint(auth.bp)
     app.register_blueprint(habit.bp)
@@ -24,10 +25,12 @@ def create_app(test_config=None):
     auth.login_manager.init_app(app)
     auth.login_manager.login_view = "auth.login"
 
+
     #from habit_app import routes
     return app
 
 app = create_app()
+
 login_manager = LoginManager()
 #login_manager.init_app(app)
 #login_manager.login_view = "auth.login"
