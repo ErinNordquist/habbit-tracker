ea317cd54d83ff919c3a51355787d6028e0a55eb
Erin Nordquist <nordquist.erin.ke@gmail.com>
2021-06-28 22:15:44 -0400

    added log in and log out functions

--- habit_app/client/habit-tracker-ui/src/App.js
+++ habit_app/client/habit-tracker-ui/src/App.js
@@ -3,18 +3,26 @@ import {useState} from "react";
 import HabitTable from "./components/HabitTable";
 import CreateUserForm from "./components/CreateUserForm";
 import LoginForm from "./components/LoginForm";
+
 //import NavBar from "./components/NavBar";
 import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import AuthActions from "./actions/AuthActions";
 import {StyledButton} from "./components/styles"
 
-function App() {
-    AuthActions.logout();
-    console.log(localStorage);
-    const [user, setUser] = useState(AuthActions.getCurrentUser());
-    const [loggedIn, setLoggedIn] = useState(user !== null);
-    console.log(user);
-    console.log(loggedIn);
+function App(props) {
+    //AuthActions.logout();
+    //console.log(AuthActions.getCurrentUser());
+    //const [user, setUser] = useState();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           AuthActions.getCurrentUser());
+    const [loggedIn, setLoggedIn] = useState(AuthActions.getCurrentUser() !== null);
+
+    const logInUser = () => {
+        //setUser(username);
+        setLoggedIn(AuthActions.getCurrentUser() !== null);
+    }
+    const logOutUser = () => {
+        AuthActions.logout();
+        setLoggedIn(AuthActions.getCurrentUser() !== null);
+    }
 
   return (
     <div className="App">
@@ -31,7 +39,9 @@ function App() {
                     <Link to="/home">
                         <StyledButton>Home</StyledButton>
                     </Link>
-
+                    <Link to="/auth/login">
+                        <StyledButton onClick={logOutUser}>Log Out</StyledButton>
+                    </Link>
                 </nav>
             </div>
             <div>
@@ -39,12 +49,13 @@ function App() {
                     <Route path="/auth/create-account">
                         <CreateUserForm/>
                     </Route>
-                    <Route props = {setUser} path="/auth/login" exact>
-                        <LoginForm/>
+                    <Route  path="/auth/login" exact>
+                        <LoginForm logInUser = {logInUser} loggedIn={loggedIn}/>
                     </Route>
                     <Route path="/home">
-                        <HabitTable/>
+                        <HabitTable loggedIn = {loggedIn} logOutUser={logOutUser}/>
                     </Route>
+
                 </Switch>
             </div>
         </BrowserRouter>
