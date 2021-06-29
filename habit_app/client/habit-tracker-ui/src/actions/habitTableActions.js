90779ccf48a74d4975177b8a59f60c7552d0c771
Erin Nordquist <nordquist.erin.ke@gmail.com>
2021-06-29 07:37:21 -0400

    Added check for token expiration on all habitTableActions

--- habit_app/client/habit-tracker-ui/src/actions/habitTableActions.js
+++ habit_app/client/habit-tracker-ui/src/actions/habitTableActions.js
@@ -1,14 +1,25 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-
+import AuthActions from "./AuthActions";
+import {useHistory} from "react-router-dom";
 
 const API_URL = "http://localhost:5000/"
+
+function checkForExpiredToken(promise) {
+    promise.catch(function (error) {
+        if (error.response.status === 401) {
+            console.log('Token is Expired');
+            AuthActions.logout();
+        }
+    })
+}
 
 function getHabits(start_date, end_date) {
     //console.log('called getHabits function');
     //console.log(authHeader());
 
     const promise = axios.get(API_URL+"home/"+start_date+"&"+end_date, {headers: authHeader()});
+    checkForExpiredToken(promise);
     return promise;
 };
 
@@ -16,9 +27,11 @@ function updateHabitAction(habit_id, habit_action, action) {
     const requestURL = API_URL + "update/"+habit_id+"&"+habit_action;
     if (action === 'add') {
         const promise = axios.post(requestURL, {}, {headers: authHeader()});
+        checkForExpiredToken(promise);
         return promise;
     } else {
         const promise = axios.delete(requestURL, {headers: authHeader()});
+        checkForExpiredToken(promise);
         return promise;
     }
 }
@@ -26,6 +39,7 @@ function updateHabitAction(habit_id, habit_action, action) {
 function addHabit(habit_title){
     const requestURL = API_URL + "habit";
     const promise = axios.post(requestURL, {'habit_title': habit_title}, {headers:authHeader()});
+    checkForExpiredToken(promise);
     return promise;
 }
 
