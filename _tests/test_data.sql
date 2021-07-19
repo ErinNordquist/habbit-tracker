5b93037adfc609a6ae7a79fc76335db275216ec5
Erin Nordquist <nordquist.erin.ke@gmail.com>
2021-07-19 07:46:22 -0400

    deleted dead testing code

--- _tests/test_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO USER (username)
-values
-('saved_test_user');
-
-INSERT INTO PASSWORD (username, password_hash)
-values
-('saved_test_user','sha256$FSTHLbw1$9479333af8bfbe1c10b88c9ec44c89d259336adbab8b760b214d05b6126d0529');
