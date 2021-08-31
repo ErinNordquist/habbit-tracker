f32ba9e05443f6f448523d7a0798471624f55720
Erin Nordquist <nordquist.erin.ke@gmail.com>
2021-08-31 07:41:23 -0400

    Raised state values and set up from HabitTable to HomePage

--- habit_app/client/habit-tracker-ui/src/components/HabitTable.js
+++ habit_app/client/habit-tracker-ui/src/components/HabitTable.js
@@ -7,51 +7,6 @@ import {Slide, Card, CardContent, Typography, Box} from '@material-ui/core';
 import '../css/HabitTable.css';
 
 function HabitTable(props) {
-    const [habits,setHabits] = useState([]);
-    const minusDays = (date, days) => {
-        date.setDate(date.getDate() - days);
-        return date;
-    }
-
-    const [dates, setDates] = useState([minusDays(new Date(), 6),
-        minusDays(new Date(), 5),
-        minusDays(new Date(), 4),
-        minusDays(new Date(), 3),
-        minusDays(new Date(), 2),
-        minusDays(new Date(), 1),
-        minusDays(new Date(), 0)
-    ]);
-
-    const dayRef = ['Su','M',"Tu","W","Th","F","Sa"];
-    const formatDate = (dt) => {
-        return String(dt.getFullYear())+"-"+
-            String(dt.getMonth()+1).padStart(2, 0)+ "-"+
-            String(dt.getDate()).padStart(2, 0);
-    }
-
-    const [formattedDates, setFormattedDates] = useState(dates.map(formatDate));
-
-    let history = useHistory();
-
-    const reloadHabits = () => {
-        habitTableActions.getHabits(formattedDates[0],formattedDates[6], history).then((response) => {
-            let data = response.data
-            setHabits(data.habit_data);
-            //console.log(habits);
-        }).catch(function (error) {
-            //console.log(JSON.stringify(error))
-            //log out if unauthorized (expired token)
-            if (error.response.status === 401) {
-                props.logOutUser()
-                history.push('/auth/login')
-            }
-        });
-    }
-
-    useEffect(() =>{
-        reloadHabits()
-    },[]);
-
     return (
         <div id = "HabitTableDiv">
                <table id='HabitTable'>
@@ -59,20 +14,22 @@ function HabitTable(props) {
                         <tr rowSpan="1" id="HeaderRow1">
                             <th rowSpan = "1" id="HabitIDHeader"></th>
                             <th rowSpan="1" id="HabitNameHeader">Habit</th>
-                            {dates.map((dt, index) => (
+                            {props.dates.map((dt, index) => (
                                 <th key = {`day${index}Header`}>
                                     <Box>
-                                        <Typography variant="body2">{dayRef[dt.getDay()]}</Typography>
+                                        <Typography variant="body2">{props.dayRef[dt.getDay()]}</Typography>
                                         <Typography variant="body1">{dt.getDate()}</Typography>
                                     </Box>
                                 </th>
                             ))}
                         </tr>
                     </thead>
-                    <HabitTableRows habits={habits} setHabits={setHabits}
-                                    formattedDates={formattedDates} history={history}/>
+                    <HabitTableRows habits={props.habits} setHabits={props.setHabits}
+                                    formattedDates={props.formattedDates} history={props.history}/>
                     <tfoot>
-                        <CreateHabitForm habits = {habits} setHabits = {setHabits} history={history}/>
+                        <CreateHabitForm habits = {props.habits}
+                                         setHabits = {props.setHabits}
+                                         history={props.history}/>
                     </tfoot>
                 </table>
         </div>
--- habit_app/client/habit-tracker-ui/src/pages/HomePage.js
+++ habit_app/client/habit-tracker-ui/src/pages/HomePage.js
@@ -1,10 +1,67 @@
 import HabitTable from "../components/HabitTable";
+import {useEffect, useState} from "react";
+import {useHistory} from "react-router-dom";
+import habitTableActions from "../actions/habitTableActions";
 
 
 function HomePage(props) {
+    const [habits,setHabits] = useState([]);
+    const minusDays = (date, days) => {
+        date.setDate(date.getDate() - days);
+        return date;
+    }
+
+    const [dates, setDates] = useState([minusDays(new Date(), 6),
+        minusDays(new Date(), 5),
+        minusDays(new Date(), 4),
+        minusDays(new Date(), 3),
+        minusDays(new Date(), 2),
+        minusDays(new Date(), 1),
+        minusDays(new Date(), 0)
+    ]);
+
+    const dayRef = ['Su','M',"Tu","W","Th","F","Sa"];
+    const formatDate = (dt) => {
+        return String(dt.getFullYear())+"-"+
+            String(dt.getMonth()+1).padStart(2, 0)+ "-"+
+            String(dt.getDate()).padStart(2, 0);
+    }
+
+    const [formattedDates, setFormattedDates] = useState(dates.map(formatDate));
+
+    let history = useHistory();
+
+    const reloadHabits = () => {
+        habitTableActions.getHabits(formattedDates[0],formattedDates[6], history).then((response) => {
+            let data = response.data
+            setHabits(data.habit_data);
+        }).catch(function (error) {
+            //log out if unauthorized (expired token)
+            if (error.response.status === 401) {
+                props.logOutUser()
+                history.push('/auth/login')
+            }
+        });
+    }
+    useEffect(() =>{
+        reloadHabits()
+    },[]);
 
     return (
-        <HabitTable loggedIn = {props.loggedIn} logOutUser={props.logOutUser}/>
+        <div>
+            <HabitTable
+                loggedIn = {props.loggedIn}
+                logOutUser={props.logOutUser}
+                habits={habits}
+                setHabits={habits}
+                dates={dates}
+                setDates={setDates}
+                formattedDates={formattedDates}
+                setFormattedDates={setFormattedDates}
+                dayRef={dayRef}
+                history={history}
+            />
+        </div>
     );
 }
 export default HomePage;
