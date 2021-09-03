208c02362d1ed1d94e1b5dea25bf9b6c0f011021
Erin Nordquist <nordquist.erin.ke@gmail.com>
2021-09-03 14:51:18 -0400

    created basic test to test title appears

--- habit_app/client/habit-tracker-ui/src/test/App.test.js
+++ habit_app/client/habit-tracker-ui/src/test/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import '@testing-library/jest-dom';
+import { unmountComponentAtNode } from "react-dom";
+let container = null;
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
 });
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+
+describe('my app', () => {
+  test('loads title', () => {
+  render(<App />);
+  const titleText = screen.getByText(/Habit Tracker/i);
+  expect(titleText).toBeInTheDocument();
+  });
+});
+
--- habit_app/client/habit-tracker-ui/src/test/setupTests.js
+++ habit_app/client/habit-tracker-ui/src/test/setupTests.js
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { unmountComponentAtNode } from "react-dom";
+
+//the below is from: https://reactjs.org/docs/testing-recipes.html
+// let container = null;
+// beforeEach(() => {
+//     // setup a DOM element as a render target
+//     container = document.createElement("div");
+//     document.body.appendChild(container);
+// });
+//
+// afterEach(() => {
+//     // cleanup on exiting
+//     unmountComponentAtNode(container);
+//     container.remove();
+//     container = null;
+// });
